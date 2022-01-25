b91a5d52ee59e5031079ba7c9377c2d66aed39f2
Henri <32584105+OctopusPie@users.noreply.github.com>
2022-01-25 11:24:01 +0100

    Create Equipment Map

---
+++ MSPR-Go-Securi-Code/src/main/java/models/EquipmentMap.java
@@ -0,0 +1,11 @@
+package models;
+import java.util.HashMap;
+
+public class EquipmentMap {
+    public static HashMap<String, String> EquimentMap() {
+        HashMap<String, String> map = new HashMap<>();
+        
+
+        return map;
+    }
+}
