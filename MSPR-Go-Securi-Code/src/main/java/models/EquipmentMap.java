1dff2d147787c4fc5cccb3e83ceb578730396ae9
Henri <32584105+OctopusPie@users.noreply.github.com>
2022-01-25 11:56:36 +0100

    Update EquipmentMap

--- MSPR-Go-Securi-Code/src/main/java/models/EquipmentMap.java
+++ MSPR-Go-Securi-Code/src/main/java/models/EquipmentMap.java
@@ -10,7 +10,8 @@ public class EquipmentMap {
         String[] fileString = Files.readString(fileName).split("\n");
         HashMap<String, String> map = new HashMap<>();
         for (int i = 0; i < fileString.length; i++) {
-
+            String[] lineString = fileString[i].split( " ", 1);
+            map.put(lineString[0], lineString[1]);
         }
         return map;
     }
