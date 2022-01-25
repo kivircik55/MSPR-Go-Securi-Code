88f757a83512535f48649f935a161faa15cb69b0
Olivier Bastin <bastin.olivier77@gmail.com>
2022-01-25 15:04:29 +0100

    Hashmap Equipment débug

--- MSPR-Go-Securi-Code/src/main/java/models/EquipmentMap.java
+++ MSPR-Go-Securi-Code/src/main/java/models/EquipmentMap.java
@@ -5,14 +5,25 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class EquipmentMap {
-    public static HashMap<String, String> EquimentMap() throws IOException {
+    public static HashMap<String, String> equimentMap() throws IOException {
         Path fileName = Path.of("C:/Users/Olivier/Documents/GitHub/Diamond_Aloha/MSPR-Go-Securi/list.txt" );
-        String[] fileString = Files.readString(fileName).split("\n");
+        String[] fileString = Files.readString(fileName).split("\\r?\\n");
         HashMap<String, String> map = new HashMap<>();
-        for (int i = 0; i < fileString.length; i++) {
-            String[] lineString = fileString[i].split( " ", 1);
-            map.put(lineString[0], lineString[1]);
+        for (String s : fileString) {
+            String[] lineString = s.split(" ");
+            StringBuilder name = new StringBuilder();
+            for (int j = 1; j < lineString.length; j++) {
+                name.append(lineString[j]).append(" ");
+            }
+            map.put(lineString[0], name.toString());
         }
         return map;
+    }
+
+    public void printaquimentmap() throws IOException {
+        HashMap<String, String> maptoprint = this.equimentMap();
+        maptoprint.entrySet().forEach(entry -> {
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        });
     }
 }
