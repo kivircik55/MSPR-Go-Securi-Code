75cc0d03c97a5eaae6764ffb2cf3a68b45aff40c
Henri <32584105+OctopusPie@users.noreply.github.com>
2022-01-25 11:50:20 +0100

    Update EquimentMap

--- MSPR-Go-Securi-Code/src/main/java/models/EquipmentMap.java
+++ MSPR-Go-Securi-Code/src/main/java/models/EquipmentMap.java
@@ -1,11 +1,17 @@
 package models;
 import java.util.HashMap;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class EquipmentMap {
-    public static HashMap<String, String> EquimentMap() {
+    public static HashMap<String, String> EquimentMap(String username) throws IOException {
+        Path fileName = Path.of("C:/Users/33614/OneDrive/Documents/GitHub/MSPR-Go-Securi/" + username);
+        String[] fileString = Files.readString(fileName).split("\n");
         HashMap<String, String> map = new HashMap<>();
-        
+        for (int i = 0; i < fileString.length; i++) {
 
+        }
         return map;
     }
 }
