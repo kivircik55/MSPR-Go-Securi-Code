7eed497e7ab7a1c54c1ba9d6b9bac68f40464af9
Olivier Bastin <bastin.olivier77@gmail.com>
2022-01-25 12:18:45 +0100

    correction path map

--- MSPR-Go-Securi-Code/src/main/java/models/EquipmentMap.java
+++ MSPR-Go-Securi-Code/src/main/java/models/EquipmentMap.java
@@ -5,8 +5,8 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class EquipmentMap {
-    public static HashMap<String, String> EquimentMap(String username) throws IOException {
-        Path fileName = Path.of("C:/Users/33614/OneDrive/Documents/GitHub/MSPR-Go-Securi/" + username);
+    public static HashMap<String, String> EquimentMap() throws IOException {
+        Path fileName = Path.of("C:/Users/Olivier/Documents/GitHub/Diamond_Aloha/MSPR-Go-Securi/list.txt" );
         String[] fileString = Files.readString(fileName).split("\n");
         HashMap<String, String> map = new HashMap<>();
         for (int i = 0; i < fileString.length; i++) {
