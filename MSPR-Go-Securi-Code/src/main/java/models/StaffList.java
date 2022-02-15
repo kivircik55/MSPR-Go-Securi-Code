28c61c6e8bb5be43286491915feca062901f8248
Olivier Bastin <bastin.olivier77@gmail.com>
2022-02-15 10:52:15 +0100

    StaffUpdate

--- MSPR-Go-Securi-Code/src/main/java/models/StaffList.java
+++ MSPR-Go-Securi-Code/src/main/java/models/StaffList.java
@@ -1,5 +1,6 @@
 package models;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -12,22 +13,19 @@ public class StaffList {
      * @return
      * @throws IOException
      */
-    
 
     public static List<String> parseStaffFromFile() throws IOException {
         Path fileName = Path.of("C:/Users/Olivier/Documents/GitHub/Diamond_Aloha/MSPR-Go-Securi/staff.txt" );
         String[] fileString = Files.readString(fileName).split("\\r?\\n");
-        List<String> map = new ArrayList<>();
-
-        for (String s : fileString) {
-            map.add(fileString[0]);
-        }
-        return map;
+        List<String> list = new ArrayList<>();
+        Collections.addAll(list, fileString);
+            return list;
     }
+
     public void printStaffList() throws IOException {
         List<String> list = parseStaffFromFile();
-        for (String print:list) {
-            System.out.println(print);
-        }
+            for (String print:list) {
+                System.out.println(print);
+            }
     }
 }
