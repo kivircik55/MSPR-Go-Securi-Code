25599b0b89d735a5a76a0a66c66905c9a72646df
Bryan Dane <kwuimobryan@gmail.com>
2022-02-18 15:16:53 +0100

    ajout des commentaires

--- MSPRGoSecuriCodeAndroid/app/src/main/java/com/example/mspr_go_securi_code_android/ListAgentsMapToAndroidThread.java
+++ MSPRGoSecuriCodeAndroid/app/src/main/java/com/example/mspr_go_securi_code_android/ListAgentsMapToAndroidThread.java
@@ -14,17 +14,22 @@ public class ListAgentsMapToAndroidThread implements Runnable {
     public static ArrayList<String> AgentListPublic;
     @Override
     public void run() {
+        //Lien vers le fichier contenant la liste des agents
         String path = "https://github.com/kivircik55/MSPR-Go-Securi/blob/main/staff.txt";
         ArrayList<String> AgentsList = new ArrayList<>();
         String[] agentsTab;
 
         try {
+            //Initialisation d'un objet representant le DOM html
             Document doc = Jsoup.connect(path).get();
 
+            //Reccuperer de l'objet contenant la liste des agents
             Element Agents = doc.select("#repo-content-pjax-container > div > div.Box.mt-3.position-relative > div.Box-body.p-0.blob-wrapper.data.type-text.gist-border-0 > div > table > tbody").first();
 
-            //Element title = doc.select("td#LC2").first();
+            //reccuperation de la liste des agents et separations de chaque mot en fonction de l'espace
             agentsTab = Agents.text().split(" ");
+
+            //ajout de la liste d'agent dans un tableau
             for (String a : agentsTab)
                 AgentsList.add(a);
             AgentListPublic = AgentsList;
